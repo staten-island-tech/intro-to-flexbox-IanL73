75ecdc74ed2b75f7622f1938d944858c02259528
IanL73 <ianl73@nycstudents.net>
2025-10-24 12:39:28 -0400

    hmmmm

--- index.js
+++ index.js
@@ -4,121 +4,141 @@ const products = [
         title: "Perfectly Generic Object",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "Perfectly generic, and perfectly useless.",
-        price: 1
+        price: 1,
+        class: "Red"
     },
     {
         title: "Notably Pointy Object",
         image: "https://i.redd.it/njnnepzm1q071.png",
         desc: "Don't step on it. If you do you'll regret it.",
-        price: 2
+        price: 2,
+        class: "Blue"
     },
     {
         title: "Probably Magical Dodecahedron",
         image: "https://i.redd.it/mr4v2bnrwp241.jpg",
         desc: "Wow, wouldja look at this thing.",
-        price: 3
+        price: 3,
+        class: "Green"
     },
     {
         title: "Miniature Jungle Gym",
         image: "https://cdn.renderhub.com/mranycad/wireframe-icosahedron/wireframe-icosahedron-01.jpg",
         desc: "You probably shouldn't use THIS for your D&D games...",
-        price: 4
+        price: 4,
+        class: "Yellow"
     },
     {
         title: "Perfectly Generic Spheroid",
         image: "https://th.bing.com/th/id/R.7a9f155f8fb29b4ae07f8a6f1f79a505?rik=WmeTslCSTskv6g&riu=http%3a%2f%2fvbs.arachnoid.com%2fblender_make_a_planet%2fgraphics%2ftutor02.png&ehk=Od9oCYReru9%2bDSib%2bMJsJ8PThXIDylyjkl8th62Z4vM%3d&risl=&pid=ImgRaw&r=0",
         desc: "Perfectly generic, and perfectly useless. Now in ball form!",
-        price:5
+        price:5,
+        class: "Eyes"
     },
     {
         title: "3D Modeling Inside Joke",
         image: "https://preview.free3d.com/img/2016/04/2269198088818132027/ngl2w9n3.jpg",
         desc: "Pip pip cherio it's tea time luve tata Bri ish",
-        price:6
+        price:6,
+        class: "Red"
     },
     {
         title: "Ford f-150",
         image: "https://tse2.mm.bing.net/th/id/OIP.gks7JyrzVOXDEgBfeyfcrQHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
         desc: "In 1975, the F-150 was introduced in between the F-100 and t",
-        price:7
+        price:7,
+        class: "Blue"
     },
     {
         title: "Pastel Pink Cone",
         image: "https://upload.wikimedia.org/wikipedia/commons/1/10/Blender-mesh-cone.png",
         desc: "You probably don't want to step on this one either...",
-        price:8
+        price:8,
+        class: "Green"
     },
     {
         title: "Vertical Slab",
         image: "https://minecraft-max.net/upload/iblock/5f5/g0srejqv72k3keztt94rc0bt2wvu1a3h.png",
         desc: "I don't see what the fuss is about, honestly.",
-        price:9
+        price:9,
+        class: "Yellow"
     },
     {
         title: "Non Functional Token",
         image: "https://techterms.com/img/xl/right-click_440.jpg",
         desc: "Why would you ever buy this? Just, Why?? It's just an image!",
-        price:10
+        price:10,
+        class: "Eyes"
     },
     {
         title: "Four Dimensional Hypercube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/8-cell-simple.gif/250px-8-cell-simple.gif",
         desc: "Perfectly generic, for four dimensional beings.",
-        price:11
+        price:11,
+        class: "Red"
     },
     {
         title: "A Second Perfectly Generic Object",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "Still perfectly generic, and still perfecty useless.",
-        price:12
+        price:12,
+        class: "Blue"
     },
     {
         title: "cube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "blah blah blah blah blah blah blah blah",
-        price: 13
+        price: 13,
+        class: "Green"
     },
     {
         title: "cube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "blah blah blah blah blah blah blah blah",
-        price:14
+        price:14,
+        class: "Yellow"
     },
     {
         title: "cube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "blah blah blah blah blah blah blah blah",
-        price:15
+        price:15,
+        class: "Eyes"
     },
     {
         title: "cube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "blah blah blah blah blah blah blah blah",
-        price:16
+        price:16,
+        class: "Red"
     },
     {
         title: "cube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "blah blah blah blah blah blah blah blah",
-        price:17
+        price:17,
+        class: "Blue"
     },
     {
         title: "cube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "blah blah blah blah blah blah blah blah",
-        price:18
+        price:18,
+        class: "Green"
     },
     {
         title: "cube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "blah blah blah blah blah blah blah blah",
-        price:19
+        price:19,
+        class: "Yellow"
     },
     {
         title: "cube",
         image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Cube_with_Blender.svg/2048px-Cube_with_Blender.svg.png",
         desc: "blah blah blah blah blah blah blah blah",
-        price:20
+        price:20,
+        class: "Eyes"
     },
 ]
 
@@ -128,18 +148,32 @@ function inject(product){
     //query the html where we inject the card
     const container = document.querySelector(".shop");
     container.insertAdjacentHTML(
-        "afterbegin",
+        "beforeend",
         `<div class="item" data-title="${product.title}" price="${product.price}">
             <img class="item-image" src="${product.image}"/>
             <h2 class="item-name">${product.title}</h2>
             <h3 class="item-description">${product.desc}</h3>
             <h4 class="item-price">${product.price}$</h4>
+            <h4 class="item-class">${product.class}</h4>
             <button class="item-buy">add to cart</button>
         </div>`
     );
 }
 // Apply Inject Function for each product in object
-products.forEach((product)=> inject(product));
+function filter(){
+    const filters = document.querySelectorAll(".filter");
+    console.log(filters)
+    filters.forEach((filter) =>
+        filter.addEventListener("click", function(event) {
+            shop.innerHTML = ""
+            let criteria = event.target.closest(".filter").textContent
+            console.log(criteria)
+            let filteredproducts = products.filter((product) => product.class === criteria)
+            filteredproducts.forEach((filteredproduct)=> inject(filteredproduct))
+        })
+    );
+}
+filter();
 
 
 function getCards(){
